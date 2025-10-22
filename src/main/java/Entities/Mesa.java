0774aece70c95cd4d3e90e53120a7c1da3f2eb9f
Diegolas <diegolas.code@gmail.com>
2025-10-22 10:07:40 -0300

    crear entidad Mesa

---
+++ src/main/java/Entities/Mesa.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entities;
+import javax.persistence.*;
+
+/**
+ *
+ * @author Diegolas
+ */
+
+@Entity
+@Table(name = "mesas")
+public class Mesa {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private int numero;
+    
+    @Column(nullable = false)
+    private int capacidad;
+    
+    @Column(nullable = false)
+    private boolean ocupada;
+
+    public Mesa() {
+    }
+
+    public Mesa(int numero, int capacidad) {
+        this.numero = numero;
+        this.capacidad = capacidad;
+        this.ocupada = false;
+    }
+
+    // Getters and Setters remain the same
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public boolean isOcupada() {
+        return ocupada;
+    }
+
+    public void setOcupada(boolean ocupada) {
+        this.ocupada = ocupada;
+    }
+}
